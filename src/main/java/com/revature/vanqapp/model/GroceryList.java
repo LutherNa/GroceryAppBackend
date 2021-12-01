187fc8d377e7e9a3539509686f95a37e88173ab0
Christian Hoy <christianphoy7@gmail.com>
2021-12-01 14:24:35 -0800

    model-repository-controller

--- src/main/java/com/revature/vanqapp/model/GroceryList.java
+++ src/main/java/com/revature/vanqapp/model/GroceryList.java
@@ -1,4 +1,31 @@
 package com.revature.vanqapp.model;
 
+import com.fasterxml.jackson.annotation.*;
+import javax.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+@Table(name = "GroceryLists")
 public class GroceryList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int groceryListId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne(cascade = CascadeType.MERGE)
+    @JsonBackReference
+    private User owner;
+
+    @ManyToMany(cascade=CascadeType.MERGE,fetch=FetchType.LAZY)
+    @JsonManagedReference
+    private Set<GroceryList> list = new HashSet<>();
+
+
 }
