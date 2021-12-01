efbdeac0d79324a3542f223abd887cc1a1961cdf
Christian Hoy <christianphoy7@gmail.com>
2021-12-01 14:22:11 -0800

    model-repository-controller

--- src/main/java/com/revature/vanqapp/model/ProductData.java
+++
@@ -1,9 +0,0 @@
-package com.revature.vanqapp.model;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.Data;
-
-@Data
-@JsonIgnoreProperties({"upc", "aisleLocation", "categories", "countryOrigin", "images", "items", "temperature"})
-public class ProductData {
-}
