96d782817a5e878e815b0f25f80ae08f2b822341
Aaron <aaron.litton18@gmail.com>
2021-12-01 13:57:03 -0500

    Deserializer implemented. Added Product,Token services. Added working Auth Token. Added Product. ProductData Depreciated

--- src/main/java/com/revature/vanqapp/DevelopmentkrogergrocerylistappApplication.java
+++ src/main/java/com/revature/vanqapp/DevelopmentkrogergrocerylistappApplication.java
@@ -1,13 +1,19 @@
 package com.revature.vanqapp;
 
+import com.revature.vanqapp.service.ProductService;
+import com.revature.vanqapp.service.TokenService;
+import lombok.SneakyThrows;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DevelopmentkrogergrocerylistappApplication {
 
+	@SneakyThrows
 	public static void main(String[] args) {
 		SpringApplication.run(DevelopmentkrogergrocerylistappApplication.class, args);
+		ProductService productService = new ProductService();
+		System.out.println(productService.getProducts());
 	}
 
 }
--- src/main/java/com/revature/vanqapp/service/ProductDeserializer.java
+++ src/main/java/com/revature/vanqapp/service/ProductDeserializer.java
@@ -1,2 +1,36 @@
-package com.revature.vanqapp.service;public class ProductDeserializer {
+package com.revature.vanqapp.service;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.revature.vanqapp.model.Product;
+import com.revature.vanqapp.model.ProductData;
+
+import java.io.IOException;
+
+public class ProductDeserializer extends StdDeserializer<Product> {
+
+    public ProductDeserializer(Class<?> vc){
+        super(vc);
+    }
+    @Override
+    public Product deserialize(JsonParser parser, DeserializationContext deserializer) throws IOException {
+        Product product = new Product();
+        while(!parser.isClosed()){
+            JsonToken jsonToken = parser.nextToken();
+            if(JsonToken.FIELD_NAME.equals(jsonToken)){
+                String fieldName = parser.getCurrentName();
+                parser.nextToken();
+                if("productId".equals(fieldName)){
+                    product.setProductId(parser.getValueAsString());
+                }else if ("brand".equals(fieldName)){
+                    product.setBrand(parser.getValueAsString());
+                }else if ("description".equals(fieldName)){
+                    product.setDescription(parser.getValueAsString());
+                }
+            }
+        }
+        return product;
+    }
 }
