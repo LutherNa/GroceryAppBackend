65ce861c4c31d6d46f73fd5ddd630588a13a4f5c
Aaron <aaron.litton18@gmail.com>
2021-12-03 12:54:48 -0500

    Location functioning

--- src/main/java/com/revature/vanqapp/controller/LocationController.java
+++ src/main/java/com/revature/vanqapp/controller/LocationController.java
@@ -1,10 +1,44 @@
 package com.revature.vanqapp.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.revature.vanqapp.model.Location;
+import com.revature.vanqapp.model.LocationFilterTerms;
+import com.revature.vanqapp.service.LocationService;
+import com.revature.vanqapp.service.ProductService;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
+import static com.revature.vanqapp.NotAKrogerApp.tokenPool;
 
 @RestController()
 @RequestMapping(value = "/location")
 public class LocationController {
 
+    private LocationService locationService;
+
+    public LocationController(){
+        try{
+            locationService = new LocationService(tokenPool);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @GetMapping
+    public List<Location> getLocationByZip(@RequestBody HashMap<LocationFilterTerms, String> search) throws IOException {
+        /*
+        HashMap<LocationFilterTerms, String> search = new HashMap<>();
+        search.put(LocationFilterTerms.zipCode,zipCode.toString());
+         */
+        return locationService.getLocations(search);
+    }
+
+    @PostMapping("/{locationId}")
+    public  List<Location> getLocationById(@PathVariable String locationId) throws IOException {
+        HashMap<LocationFilterTerms, String> search = new HashMap<>();
+        search.put(LocationFilterTerms.locationId, locationId);
+        return locationService.getLocations(search);
+    }
 }
