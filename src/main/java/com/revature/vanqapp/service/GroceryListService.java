44bde550fd9cbd2a245e3d641d1ecb19df4315d4
Christian Hoy <christianphoy7@gmail.com>
2021-12-07 07:46:11 -0800

    Temp-GroceryList-Commit

--- src/main/java/com/revature/vanqapp/service/GroceryListService.java
+++ src/main/java/com/revature/vanqapp/service/GroceryListService.java
@@ -3,6 +3,8 @@ package com.revature.vanqapp.service;
 import com.revature.vanqapp.model.AuthToken;
 import com.revature.vanqapp.model.GroceryList;
 import com.revature.vanqapp.model.User;
+import com.revature.vanqapp.model.product.Product;
+import com.revature.vanqapp.model.product.ProductFilterTerms;
 import com.revature.vanqapp.repository.GroceryListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 
 @Service
@@ -23,6 +26,9 @@ public class GroceryListService {
     @Autowired
     UserService userService;
 
+    @Autowired
+    ProductService productService;
+
     public GroceryListService() throws IOException {
         authToken = TokenService.getToken();
     }
@@ -34,5 +40,10 @@ public class GroceryListService {
     public GroceryList createGroceryList(String name, String locationId, Integer userId){
         GroceryList groceryList = new GroceryList(name, locationId, userService.findUserById(userId));
         return groceryListRepository.save(groceryList);
+    }
+
+    public GroceryList addProductToGroceryList(String name, Integer userId, HashMap<ProductFilterTerms,String> searchMap) throws IOException {
+        if ((productService.getProductsByIdAndLocation(searchMap).getClass()) == Product.class);
+            return new GroceryList();
     }
 }
