31111a6abb8ffbdc6c5c648f4144320d870c2fa1
Nathan Luther <nathan.luther@revature.net>
2021-12-10 14:55:26 -0500

    trying to get cors to work

--- src/main/java/com/revature/vanqapp/controller/GroceryListController.java
+++ src/main/java/com/revature/vanqapp/controller/GroceryListController.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 
-@RestController()
+@RestController @CrossOrigin("*")
 @RequestMapping(value = "/grocerylist")
 public class GroceryListController {
 
