76c84a496a1d55646bb6a89b5089050307ec70e4
Aaron <aaron.litton18@gmail.com>
2021-12-10 12:08:14 -0500

    Added login functionality to front-end

--- webapp/apiXML.js
+++ webapp/apiXML.js
@@ -1,38 +1,77 @@
 
 const apiBaseUrl = 'http://localhost:8081/api'
 
+$(window, document, undefined).ready(function() {
+    $('.input').blur(function() {
+      var $this = $(this);
+      if ($this.val())
+        $this.addClass('used');
+      else
+        $this.removeClass('used');
+    });
+});
+  
+  
+$('#tab1').on('click' , function(){
+    $('#tab1').addClass('login-shadow');
+    $('#tab2').removeClass('signup-shadow');
+});
+  
+$('#tab2').on('click' , function(){
+    $('#tab2').addClass('signup-shadow');
+    $('#tab1').removeClass('login-shadow');
+});
+
+
+
+
+
 function locButton () {
     getLocations()
-    //$("button").on("click",function(){
-    //console.log("hii");
-    // var data = {"zipCode":"37601"};
-    // $.ajax({
-    //     type: "Post",
-    //     url:apiBaseUrl + "/location",
-    //     data: JSON.stringify(data),
-    //     headers:{
-    //         "Accept":"application/json",//depends on your api
-    //         "Content-type":"application/Json"//depends on your api
-    //     },   
-    //     success:function(response){
-    //         var r = response;
-    //         console.log(r)
-    //         for(let location of r){
-    //             var hold = document.createElement(location.tag);
-    //             hold.innerHTML = "<p> Name: " + location.name + ", Address: " + location.address.addressLine1 + " " + location.address.city + "</p>";
-    //             document.getElementById("locationList").appendChild(hold);
-    //             console.log(location.name);
-    //         }
-    //     }
-    // });
+}
 
+let User = class {
+    constructor(username, password){
+        this.username = username,
+        this.password = password
+    };
+
+    setId(id) {
+        this.id = id;
+    }
+}
+
+var user = new User(undefined,undefined);
+
+function hide (elements) {
+    elements = elements.length ? elements : [elements];
+    for (var index = 0; index < elements.length; index++) {
+      elements[index].style.display = 'none';
+    }
+}
+
+function show (elements) {
+    elements = elements.length ? elements : [elements];
+    for (var index = 0; index < elements.length; index++) {
+      elements[index].style.display = 'block';
+    }
+}
+
+document.addEventListener("click", myFunction);
+function myFunction() {
+    if(user.username != undefined && user.password != undefined){
+        show(document.getElementById("navBar"));
+    } else {
+        hide(document.getElementById("navBar"));
+    }
 }
 
 function renderLocations(locations){
     while(locationList.firstChild) locationList.removeChild(firstChild);
     locations.forEach(location => {
         var hold = document.createElement(location.tag)
-        hold.innerHTML = "<p> Name: " + location.name + ", Address: " + location.address.addressLine1 + " " + location.address.city + "</p>";
+        hold.innerHTML = "<p> Name: " + location.name +
+                         ", Address: " + location.address.addressLine1 + " " + location.address.city + "</p>";
         document.getElementById("locationList").appendChild(hold)
         console.log(location.name)
     })
@@ -54,24 +93,4 @@ function getLocations(){
     xhr.send(JSON.stringify(data))
 
 }
-
-function buildLocations(tree, container) {  
-    tree.forEach(function(node) {
-  
-      var el = document.createElement(node.tag);
-  
-      if (Array.isArray(node.content)) {
-          buildLocations(node.content, el);
-      } 
-      else if (typeof(node.content) == 'object') {
-          buildLocations([node.content], el);
-      }
-      else {
-          el.innerHTML = node.content;
-      }
-  
-      container.appendChild(el);
-    });
-}
-
  
